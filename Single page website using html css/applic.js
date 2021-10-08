8d8dffe5f18835c75b84bd634b7c9c94aedfab79
A-Loukya <adepuloukya1@gmail.com>
2021-10-08 19:32:00 +0530

    added files

---
+++ Single page website using html css/applic.js
@@ -0,0 +1,7 @@
+const btn=document.querySelector(".java");
+const ul=document.querySelector(".script");
+// const active=document.querySelector(".active");
+
+btn.addEventListener('click',function(){
+    ul.classList.toggle("active");
+})
